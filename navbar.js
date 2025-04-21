b5e73707744b30d38310e1e808fedd05ecae388f
Hazelyn Arboiz <HazelynArboiz@example.com>
2025-04-21 15:49:45 +0800

    Initial commit - Web Portfolio

---
+++ navbar.js
@@ -0,0 +1,17 @@
+//responsive navbar
+let menu = document .querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+
+
+//sticky navbar
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+}
+)
